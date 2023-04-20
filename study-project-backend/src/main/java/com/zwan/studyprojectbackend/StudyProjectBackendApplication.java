8e1c01776b2afa3cda1744ee725dfe6b3711a452
Zwanan-github <270603801@qq.com>
2023-04-20 14:14:38 +0800

    创建项目

---
+++ study-project-backend/src/main/java/com/zwan/studyprojectbackend/StudyProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.zwan.studyprojectbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyProjectBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyProjectBackendApplication.class, args);
+    }
+
+}
---
+++ study-project-backend/src/test/java/com/zwan/studyprojectbackend/StudyProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zwan.studyprojectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyProjectBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
