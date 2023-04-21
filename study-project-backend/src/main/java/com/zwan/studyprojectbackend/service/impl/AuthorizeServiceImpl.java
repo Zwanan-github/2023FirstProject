16f8e761d90c6801583e0aac19276dfd1984078e
Zwanan-github <270603801@qq.com>
2023-04-21 17:32:05 +0800

    单纯的发送邮件功能

--- study-project-backend/src/main/java/com/zwan/studyprojectbackend/service/impl/AuthorizeServiceImpl.java
+++ study-project-backend/src/main/java/com/zwan/studyprojectbackend/service/impl/AuthorizeServiceImpl.java
@@ -4,15 +4,27 @@ import com.zwan.studyprojectbackend.entity.Account;
 import com.zwan.studyprojectbackend.mapper.UserMapper;
 import com.zwan.studyprojectbackend.service.AuthorizeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailException;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Random;
+
 @Service
 public class AuthorizeServiceImpl implements AuthorizeService {
     @Autowired
     private UserMapper userMapper;
+
+    @Autowired
+    MailSender mailSender;
+
+    @Value("${spring.mail.username}")
+    String from;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -40,6 +52,19 @@ public class AuthorizeServiceImpl implements AuthorizeService {
          * 4. 发送失败， 删除Redis中的该键值对
          * 5. 用户注册时，从Redis中取出来对比
          */
-        return false;
+        Random random = new Random();
+        int code = random.nextInt(899999) + 100000;
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(from);
+        message.setTo(email);
+        message.setSubject("您的验证邮件");
+        message.setText("验证码是：" + code);
+        try{
+            mailSender.send(message);
+        } catch (MailException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
     }
 }
